70d7af54e6e1480c1ad52cc252be5941a6ed9300
Prosperoustech <prosperousnbright@gmail.com>
2025-08-01 22:11:15 +0100

    initial commit

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+const mainnav = document.querySelector(".navigation")
+const hambutton = document.querySelector("#menu")
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("show")
+    hambutton.classList.toggle("show")
+});
+
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = "Last Modification: " + document.lastModified;
